453d011943c2a5c95eb8fe3119d223251244529a
jan.bujanowski <jan.bujanowski@gmail.com>
2018-05-07 23:23:45 +0200

    some lotto fun analysis

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var filePath = @"C:\Users\janbu\Desktop\Repos\CoreConsole\lotto.txt";
+            var lines = File.ReadAllLines(filePath);
+            int[] accumulated = new int[50];
+            for (int i = 0; i < accumulated.Length; i++)
+            {
+                accumulated[i] = 0;
+            }
+            accumulated[0] = 99999;
+            foreach (var line in lines)
+            {
+                var numbers = line.Split(' ')[2].Split(',');
+                foreach (var numba in numbers)
+                {
+                    var id = Int32.Parse(numba);
+                    accumulated[id]++;
+                }
+            }
+            var accList = accumulated.Min(); 
+            var min = accumulated.Min();
+
+            for (int i = 0; i < accumulated.Length; i++)
+            {
+                System.Console.WriteLine($"Liczba {i} wystąpień : {(accumulated[i] - min)}"); ;
+            }
+
+        }
+    }
+}
